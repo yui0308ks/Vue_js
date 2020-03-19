5200618ce01eb4f9485b408e90bf79cbdb1e6235
yui0308ks <kazuyu03081@gmail.com>
2020-03-19 22:27:17 +0900

    v-onでイベント設定

--- js/main.js
+++ js/main.js
@@ -1,16 +1,21 @@
 (function() {
   'use strict';
 
-  // two way data binding (to UI)
-
   var vm = new Vue({
     el: '#app',
     data: {
+      newItem: '',
       todos: [
         'task 1',
         'task 2',
         'task 3'
       ]
+    },
+    methods: {
+      addItem: function(e) {
+        this.todos.push(this.newItem);
+        this.newItem = '';
+      }
     }
   })
 })();
